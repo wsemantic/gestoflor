889da5941875fa2ca474e8e70c8a45a71e3b8dca
Alfonso Gonzalez <info@wsemantic.com>
2024-07-08 18:23:30 +0200

    Update models.py

--- models/models.py
+++ models/models.py
@@ -11,7 +11,9 @@ class SaleOrder(models.Model):
         for order in self:
             if not order.commitment_date:
                 raise UserError('La fecha de entrega no está definida. Por favor, defina una fecha de entrega antes de confirmar el pedido.')
-        
+            if not order.fsm_location_id:
+                raise UserError('La ubicación FSM no está definida. Por favor, defina una ubicación FSM antes de confirmar el pedido.')
+
         res = super(SaleOrder, self).action_confirm()
         _logger.info(f'WSEM fsm action_confirm')
         
@@ -26,21 +28,23 @@ class SaleOrder(models.Model):
                         equipments = self.env['fsm.equipment'].search([('product_id.product_tmpl_id', '=', product_tmpl_id)])
                         for equipment in equipments:
                             _logger.info(f'WSEM fsm iterando equipo')
-                            self._create_stock_request_for_equipment(fsm_order, equipment, order.commitment_date)
+                            self._create_stock_request_for_equipment(fsm_order, equipment, order.commitment_date, order.fsm_location_id.id)
         return res
 
-    def _create_stock_request_for_equipment(self, fsm_order, equipment, expected_date):
+    def _create_stock_request_for_equipment(self, fsm_order, equipment, expected_date, location_id):
         self.env['stock.request'].create({
             'fsm_order_id': fsm_order.id,
             'product_id': equipment.product_id.id,
             'product_uom_qty': 1,
             'state': 'draft',  # Assuming 'draft' is the initial state
             'expected_date': expected_date,
-            'direction':'outbound',
-            'picking_policy':'one'
+            'location_id': location_id,
+            'direction': 'outbound',
+            'picking_policy': 'one'
         })
         for child in equipment.child_ids:
             _logger.info(f'WSEM fsm add child')
-            self._create_stock_request_for_equipment(fsm_order, child, expected_date)
+            self._create_stock_request_for_equipment(fsm_order, child, expected_date, location_id)
+
 
 
