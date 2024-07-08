fdf1e6f48498c8a58f1a8255a0eb7f0d2fb5de5c
Alfonso Gonzalez <info@wsemantic.com>
2024-07-08 17:12:32 +0200

    Update models.py

--- models/models.py
+++ models/models.py
@@ -3,27 +3,27 @@ from odoo import models, fields, api
 
 _logger = logging.getLogger(__name__)
 
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
 
-    @api.model
-    def create(self, vals):
-        _logger.info(f'WSEM fsm creando linea')
-        res = super(SaleOrderLine, self).create(vals)
-        if res.product_id:
-            # Buscar equipos asociados a cualquier variante del template del producto
-            product_tmpl_id = res.product_id.product_tmpl_id.id
-            equipments = self.env['fsm.equipment'].search([('product_id.product_tmpl_id', '=', product_tmpl_id)])
-            for equipment in equipments:
-                _logger.info(f'WSEM fsm iterando equipo')
-                self._add_equipment_to_fsm_order(res.order_id, equipment)
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+        _logger.info(f'WSEM fsm action_confirm')
+        for order in self:
+            fsm_order = self.env['fsm.order'].search([('sale_id', '=', order.id)], limit=1)
+            if fsm_order:
+                _logger.info(f'WSEM fsm orden {fsm_order.id}')
+                for line in order.order_line:
+                    if line.product_id:
+                        # Buscar equipos asociados a la variante del producto
+                        equipments = self.env['fsm.equipment'].search([('product_id', '=', line.product_id.id)])
+                        for equipment in equipments:
+                            _logger.info(f'WSEM fsm iterando equipo')
+                            self._add_equipment_to_fsm_order(fsm_order, equipment)
         return res
 
-    def _add_equipment_to_fsm_order(self, sale_order, equipment):
-        fsm_order = self.env['fsm.order'].search([('sale_id', '=', sale_order.id)], limit=1)
-        if fsm_order:
-            _logger.info(f'WSEM fsm add equip')
-            fsm_order.equipment_ids |= equipment
-            for child in equipment.child_ids:
-                _logger.info(f'WSEM fsm add child')
-                self._add_equipment_to_fsm_order(sale_order, child)
+    def _add_equipment_to_fsm_order(self, fsm_order, equipment):
+        fsm_order.equipment_ids |= equipment
+        for child in equipment.child_ids:
+            _logger.info(f'WSEM fsm add child')
+            self._add_equipment_to_fsm_order(fsm_order, child)
